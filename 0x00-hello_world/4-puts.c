9f6631463de49b9bfa6659801b27bbe4951f30b8
achrafedd <achrafeddiba@gmail.com>
2023-07-13 07:30:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,12 @@
 /*
  * main - Entry point
  *
- * Return: 0
+ * @description: print into the console
+ *
+ * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;

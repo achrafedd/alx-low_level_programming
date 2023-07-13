38800c2768ec56f4848545f2a9e65b116e407c4e
achrafedd <achrafeddiba@gmail.com>
2023-07-13 09:04:24 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<unistd.h>
+
+/**
+ * main - Entry piont
+ *
+ * Return: alwais 0
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}

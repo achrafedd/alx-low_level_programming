7e65cbdbc18b9d135d2a7eb52e30b5e8558c3483
achrafedd <achrafeddiba@gmail.com>
2023-07-14 14:18:25 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print lower case letter followed by upper case letters
+ *
+ * Return: 0
+ */
+
+int main(viod)
+{
+	int lower_case, upper_case;
+
+	for (lower_case = 97;lower_case <= 122; lower_case++)
+		putchar(lower_case);
+	for (upper_case = 65; upper_case <= 90; upper_case++)
+		putchar(upper_case);
+	putchar('\n');
+}

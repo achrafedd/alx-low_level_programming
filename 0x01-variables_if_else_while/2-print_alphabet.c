d331e94952b5bde805814d0d9dc10fa16e9f4b67
achrafedd <achrafeddiba@gmail.com>
2023-07-14 09:26:07 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main: Entry piont
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}

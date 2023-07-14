b3f98958974e2a1b755b6e7817a2d81153f2c6e6
achrafedd <achrafeddiba@gmail.com>
2023-07-14 14:28:18 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int lower_case, upper_case;
 
-	for (lower_case = 97;lower_case <= 122; lower_case++)
-		putchar(lower_case); 
-	for (upper_case = 65; upper_case <= 90; upper_case++)
-		putchar(upper_case); 
+	for (lower_case = 97;lower_case <= 122; lower_case++) 
+		putchar(lower_case);
+	for (upper_case = 65; upper_case <= 90; upper_case++) 
+		putchar(upper_case);
 	putchar('\n');
 	return (0);
 }

d6141b27c2d936eb15f70682ea6fadee8b4019a1
achrafedd <achrafeddiba@gmail.com>
2023-07-14 15:25:28 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j;
+	
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	for (j = 97; j <= 102; i++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}

ff6e696ec76cfddfd27a4171c7de728c7d0154f8
achrafedd <achrafeddiba@gmail.com>
2023-07-14 16:31:24 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48, i <= 55; i++)
+	{
+		for (j = 48; j <= 56; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				if (k > j && j > i)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 55 || j != 56) 
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

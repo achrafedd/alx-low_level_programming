85ec735abf065d85ada5f97e6ecabb4026e9f0c9
achrafedd <achrafeddiba@gmail.com>
2023-07-18 12:26:25 -0700

    add function that return the abslute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @n: number
+ *
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (n * -1);
+}

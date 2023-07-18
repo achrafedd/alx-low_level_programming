06d305712bddd7b9aeb67ba3a0008c85e8e0eb44
achrafedd <achrafeddiba@gmail.com>
2023-07-18 14:37:58 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,11 +7,11 @@
  */
 void jack_bauer(void)
 {
-	int h, m = 0;
+	int h, m;
 
-	for (; h <= 23; h++)
+	for (h = 0; h <= 23; h++)
 	{
-		for (; m <= 59; m++)
+		for (m = 0; m <= 59; m++)
 		{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');

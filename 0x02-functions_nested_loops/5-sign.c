337cf13545d819f7eba3367f859403c63badfa19
achrafedd <achrafeddiba@gmail.com>
2023-07-18 11:44:12 -0700

    add function print the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of number
+ *
+ * @n: number
+ *
+ * Return: 1 if number is grater then 0 and 0 if number is 0 and -1 if
+ * number is smuller then 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}else if( n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	_putchar('0');
+	return (0);
+}

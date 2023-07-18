f6668ac8cb0b5c1df94d2e44e7014c1498e68e7f
achrafedd <achrafeddiba@gmail.com>
2023-07-18 13:35:23 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print minute of the day
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l = 0;
+	for (; i < 3; i++)
+	{
+		for (; j < 4; j++)
+		{
+			for (; k < 6; k++)
+			{
+				for (; l < 10; l++)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}

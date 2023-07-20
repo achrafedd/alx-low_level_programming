2f1eeb0a7cb136ea245c8c39266ef2d7dbb1b09a
achrafedd <achrafeddiba@gmail.com>
2023-07-20 12:26:59 -0700

    fix tasks

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,16 +2,16 @@
 
 /**
  * _isupper - check character if it's upper
- * 
+ *
  * @c: the character to check
- * 
+ *
  * Return: 1 if it's upper or 0 if it's lower
  */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-    {
-        return (1);
-    }
-    return (0);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
 }

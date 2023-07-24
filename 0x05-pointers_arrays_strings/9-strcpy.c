861534b1f0366a6c13214240eeba0e4d86d0c153
achrafedd <achrafeddiba@gmail.com>
2023-07-24 10:28:16 -0700

    task 9: copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - copy a string into onther
+ *
+ * @dest: the copeid string
+ * @src: the original string
+ *
+ * Return: the copeid string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, len;
+
+	len = strlen(src);
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest += '\0';
+	return (dest);
+}

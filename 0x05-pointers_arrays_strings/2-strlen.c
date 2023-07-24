8a082f18c4e21fae0313b7e763fa6fbae1002c9d
achrafedd <achrafeddiba@gmail.com>
2023-07-24 06:25:14 -0700

    task 2: string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - count numbers of letters in string
+ *
+ * @s: string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	while (*s[i] != '\0')
+	{
+		i++
+	}
+	return (i);
+}

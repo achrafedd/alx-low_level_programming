1cf6d5ec34bb20c021bba36c999acb18f6ea1992
achrafedd <achrafeddiba@gmail.com>
2023-07-24 08:40:54 -0700

    task 6:

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,12 +11,9 @@ void puts2(char *str)
 {
 	int i;
 
-	i = 0;
-
-	while (str[i] != '\0' || str[i] == '\\')
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
-		_putchar(str[i]);
-		i += 2;
+		_putchar (str[i]);
 	}
 	_putchar('\n');
 }

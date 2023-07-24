fc260297ee82120ea561d5ffbb929bf8fb8b314b
achrafedd <achrafeddiba@gmail.com>
2023-07-24 07:12:01 -0700

    task 5: reversing string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reversing the string
+ *
+ * @s: the string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char new_s[_strlen(s)];
+	int i, j;
+
+	i = _strlen(s) - 1;
+	j = 0;
+
+	while (i >= 0)
+	{
+		new_s[j] = s[i];
+		i--;
+		j++;
+	}
+
+	s = new_s;
+}

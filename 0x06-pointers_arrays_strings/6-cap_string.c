66a9cc1821da4ea2feb10be52f482631c5ed9f04
achrafedd <achrafeddiba@gmail.com>
2023-07-26 15:41:43 +0100

    task 6: capitalize a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize the string
+ *
+ * @s: string
+ *
+ * Return: the capitalized string
+*/
+char *cap_string(char *s)
+{
+	int i, j;
+	char seq[14] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	if (s[0] >= 97 && s[0] <= 122)
+		s[0] -= 32;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] >= 97 && s[i] <= 122 && s[i - 1] == seq[j])
+			{
+				s[i] -= 32;
+			}
+		}
+	}
+	return (s);
+}

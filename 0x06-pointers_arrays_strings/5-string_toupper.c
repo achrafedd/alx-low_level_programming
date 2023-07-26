73a32548666283a5e8f3e5a80d4059905c068546
achrafedd <achrafeddiba@gmail.com>
2023-07-26 15:16:04 +0100

    task 5: uppercase string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - uppercase the string
+ *
+ * @s: string
+ *
+ * Return: the uppercase string
+*/
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] -= 32;
+		}
+	}
+	return (s);
+}

cbbbbd883e246470eda1aa4be4488b3a8dbb9c7b
achrafedd <achrafeddiba@gmail.com>
2023-07-27 20:32:43 +0100

    fix task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,36 +10,41 @@
 */
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int o, j, i;
 
-	if (size < 0)
+	o = 0;
+
+	if (size <= 0)
 	{
 		printf("\n");
 		return;
 	}
-
-	for (i = 0; i < size; i+= 10)
+	while (o < size)
 	{
-		if (i % 10 == 0)
-			printf("%08x: ", i);
-		for (j = i; j < i + 9; j += 2)
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
 		{
-			if ((j < size) && ((j + 1) < size))
-				printf("%02x%02x: ", b[j], b[j + 1]);
+			if (i < j)
+				printf("%02x", *(b + o + i));
 			else
+				printf("  ");
+			if (i % 2)
 			{
-				while (++j <= i + 10)
-					printf(" ");
 				printf(" ");
 			}
 		}
-		for (j = i; j < i + 9 && j < size; j++)
+		for (i = 0; i < j; i++)
 		{
-			if (b[j] >= 32 && b[j] <= 126)
-				printf("%c", b[j]);
-			else
-				printf(".");
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
 		}
 		printf("\n");
+		o += 10;
 	}
 }

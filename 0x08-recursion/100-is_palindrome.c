7c61048848db426720dc6c5ee58a57bf2083cda2
achrafedd <achrafeddiba@gmail.com>
2023-08-02 22:06:20 +0100

    task 7: string is a palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+int palindrome(char *s, int i, int j)
+{
+	if (s[i] == s[j])
+		if (i > j / 2)
+			return (1);
+		else
+			return (palindrome(s, i + 1, j - 1));
+	else
+		return (0);
+}
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+ *
+ * @s: string
+ *
+ * Return: 1 if a string is a palindrome and 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	if (strlen(s) == 0)
+		return (1);
+
+	return (palindrome(s, 0, strlen(s) - 1));
+}

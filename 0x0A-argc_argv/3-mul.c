a91aec976357c630bb104757e6a0423644d333bf
achrafedd <achrafeddiba@gmail.com>
2023-08-04 19:53:29 +0100

    task 3: multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ *
+ * @argc: length of @argv
+ * @argv: array of elements
+ *
+ * Return: 0 Alwais
+ */
+int main(int argc, char *argv[])
+{
+	char n1, n2;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	printf("%d\n", n1 * n2);
+	return (0);
+}

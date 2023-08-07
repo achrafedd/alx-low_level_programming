614d62a82e0576fd420e57803ff76dc158abad56
achrafedd <achrafeddiba@gmail.com>
2023-08-07 16:49:52 +0100

    task 0: create an array whit malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create an array whit malloc
+ *
+ * @size: the size of the array
+ * @c: data stored in the array
+ *
+ * Return: a pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *x = (char *)malloc(size);
+
+	if (size == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		x[i] = c;
+
+	return (x);
+}

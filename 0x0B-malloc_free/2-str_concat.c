1296fafaab50326dea2241a9b7f6812e1db87e4b
achrafedd <achrafeddiba@gmail.com>
2023-08-07 20:13:26 +0100

    task 2: concatinate two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - concatinate two strings
+ *
+ * @s1: string
+ * @s2: string
+ *
+ * Return: concatinated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i, len, s1_len, s2_len;
+	char *str;
+
+	if (s1 == NULL || s2 == NULL)
+		return ("");
+
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
+	len = s1_len + s2_len;
+	str = (char *)malloc(len + 1);
+
+	if (!str)
+		return (NULL);
+
+	for (i = 0; i < s1_len; i++)
+	{
+		str[i] = s1[i];
+	}
+
+	for (i = 0; i < s2_len; i++)
+	{
+		str[s1_len] = s2[i];
+		s1_len++;
+	}
+
+	str += '\0';
+
+	return (str);
+}

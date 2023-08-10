1d9e0a6986a303f29e8d2c71ebfa12ba369d6c38
achrafedd <achrafeddiba@gmail.com>
2023-08-10 19:25:44 +0100

    task 2: allocates memory for an array, using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,6 +11,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *x;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -19,6 +20,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (!x)
 		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		x[i] = 0;
 
 	return (x);
 }

b8ee20090994360de40cd931e95f9a018955aff7
achrafedd <achrafeddiba@gmail.com>
2023-08-14 16:23:58 +0100

    task 3: defined a new type of dog dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,12 @@ struct dog
 	float age;
 };
 
+/**
+ * dog_t - type of dog
+ */
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 

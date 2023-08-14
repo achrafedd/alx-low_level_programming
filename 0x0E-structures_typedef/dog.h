7e6fc3ee7b79bd061f4155e7d55e1b371527f49e
achrafedd <achrafeddiba@gmail.com>
2023-08-14 15:43:52 +0100

    task 0: create a dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+/**
+ * struct dog - a dog struct
+ *
+ * @name: dog name
+ * @owner: dog owner
+ * @age: dog age
+ *
+ * Description: a dog struct
+*/
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};

ee7898ec4c70c419349a4a69559079cb1eda02c8
achrafedd <achrafeddiba@gmail.com>
2023-08-17 18:40:40 +0100

    task 1: print all numbers in the arguments

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print all numbers in arguments
+ *
+ * @separator: separator of number
+ * @n: arguments count
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	if (n == 0)
+		return;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		int x = va_arg(args, int);
+
+		printf("%d", x);
+		if(separator != NULL && i + 1 != n)
+			printf("%s", separator);
+	}
+	printf("\n");
+}

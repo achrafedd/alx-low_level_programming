b4383083c72076775c22f77a3d55e6d8a7a3d925
achrafedd <achrafeddiba@gmail.com>
2023-08-17 19:09:40 +0100

    task 2: print all arguments

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * 
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		char* x = va_arg(args, char *);
+
+		if (x != NULL)
+			printf("%s", x);
+		else
+			printf("(nil)");
+		if (separator != NULL && i + 1 != n)
+			printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
+}

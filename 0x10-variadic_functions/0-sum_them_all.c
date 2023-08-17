88546dd776973df3071fafbd611b22b04e109b51
achrafedd <achrafeddiba@gmail.com>
2023-08-17 18:28:54 +0100

    task 0: sum all numbers in arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,13 +13,15 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0;
 
-	if (n == 0) return (0);
+	if (n == 0)
+		return (0);
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
 		int x = va_arg(args, int);
+
 		sum += x;
 	}
 

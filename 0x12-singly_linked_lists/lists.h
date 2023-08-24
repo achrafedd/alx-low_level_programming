fc9812a3a85a3735c7afd14934fe871091c7608b
achrafedd <achrafeddiba@gmail.com>
2023-08-24 14:46:33 +0100

    task 2: add node to linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * struct list - a linked list struct
@@ -17,5 +20,6 @@ typedef struct list
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif

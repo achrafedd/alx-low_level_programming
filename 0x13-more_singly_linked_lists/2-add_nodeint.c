6510b6f32e6eec1c6fd609a9f5b102ef37afc1b5
achrafedd <achrafeddiba@gmail.com>
2023-08-28 17:06:09 +0100

    fix task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,8 +1,13 @@
 #include "lists.h"
 
 /**
- * 
-*/
+ * add_nodeint - add node to the linked list
+ *
+ * @head: head of the linked list
+ * @n: number
+ *
+ * Return: new added node
+ */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *node = (listint_t *)malloc(sizeof(listint_t));

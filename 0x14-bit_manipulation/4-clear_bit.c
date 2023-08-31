eb9215148cd028b7234c755d4931ccba97b30104
achrafedd <achrafeddiba@gmail.com>
2023-08-31 20:00:58 +0100

    task 4: clear bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * clear_bit - clear bit
+ *
+ * @n: number
+ * @index: index
+ *
+ * Return: number
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	i = ~(1 << index);
+	*n = *n & i;
+
+	return (1);
+}

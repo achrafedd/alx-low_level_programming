4ef66931c1c0e6900354d4244cdb4077a8695e95
achrafedd <achrafeddiba@gmail.com>
2023-08-31 19:43:13 +0100

    task 1: print binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <unistd.h>
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif

590634fa0e3c48b8fc71838204ac33b5baa3e7d7
achrafedd <achrafeddiba@gmail.com>
2023-10-12 17:06:18 +0100

    fix task 0

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,11 +1,20 @@
 #ifndef LISTS_H
-#define LISTD_H
+#define LISTS_H
 #include <stdio.h>
 
-typedef struct dlistint {
+/**
+ * struct dlistint - doubly linked list
+ *
+ * @n: int
+ * @next: next node
+ * @prev: previos node
+ */
+
+typedef struct dlistint
+{
 	int n;
-	struct dlistint* next;
-	struct dlistint* prev;
+	struct dlistint *next;
+	struct dlistint *prev;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);

8a155ca08ef740840db89221848412087ba5d220
achrafedd <achrafeddiba@gmail.com>
2023-10-12 19:54:20 +0100

    task 2: add dnodeint

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add node to the end of linked list
+ *
+ * @head: liked list
+ * @n: int
+ *
+ * Return: Linked list
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+
+}

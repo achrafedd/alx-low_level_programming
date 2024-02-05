6c15e052a222896f2151e07ae2f31f4a4f45e2e3
achrafedd <achrafeddiba@gmail.com>
2024-02-05 15:41:40 +0100

    task 1: binary

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+
+/**
+* binary_search - search for a value on an array with
+* binary search algorithem
+*
+* @array: the array
+* @size: the size of the array
+* @value: value to be found
+*
+* Return: index of the value in the array or -1 if value
+* not found
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	int start, mid, end, i;
+
+	if (array == NULL)
+		return (-1);
+
+	start = 0;
+	end = size - 1;
+
+	while (start < end)
+	{
+		mid = start + (end - start) / 2;
+		printf("Searching in array: ");
+		for (i = start; i < end + 1; i++) {
+			if (i == end)
+				printf("%i", array[i]);
+			else
+				printf("%i, ", array[i]);
+		}
+		printf("\n");
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] < value)
+			start = mid + 1;
+		else
+			end = mid - 1;
+	}
+	return (-1);
+}

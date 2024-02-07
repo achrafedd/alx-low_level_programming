4f63f50065fa477b839920c1f04f84fe6e5034bb
achrafedd <achrafeddiba@gmail.com>
2024-02-07 23:04:22 +0100

    task 105: jump list

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,58 @@
+#include "search_algos.h"
+
+/**
+ * move_forward - moves a list forward until the index matches a desired
+ * index, or the last node in the list
+ * @list: list to move forward
+ * @index: desired index
+ *
+ * Return: node with desired index, or last node in the list
+ */
+listint_t *move_forward(listint_t *list, size_t index)
+{
+	while (list->next != NULL && list->index < index)
+		list = list->next;
+	return (list);
+}
+
+/**
+* jump_list - search for value on linked list
+*
+* @list: list
+* @size: size of linked list
+* @value: value to be found
+*
+* Return: pointer of node or NULL
+*/
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+		size_t jump;
+	listint_t *left, *right;
+
+	if (list != NULL && size > 0)
+	{
+		jump = sqrt(size);
+		left = list;
+		right = move_forward(left, jump);
+		printf("Value checked at index [%lu] = [%d]\n", right->index, right->n);
+		while (right->index < (size - 1) && right->n < value)
+		{
+			left = right;
+			right = move_forward(left, right->index + jump);
+			printf("Value checked at index [%lu] = [%d]\n", right->index, right->n);
+		}
+		printf("Value found between indexes [%lu] and [%lu]\n",
+				left->index, right->index);
+		printf("Value checked at index [%lu] = [%d]\n", left->index, left->n);
+		while (left->index < size - 1 && left->n < value)
+		{
+			left = left->next;
+			if (left == NULL)
+				return (NULL);
+			printf("Value checked at index [%lu] = [%d]\n", left->index, left->n);
+		}
+		if (left->n == value)
+			return (left);
+	}
+	return (NULL);
+}
